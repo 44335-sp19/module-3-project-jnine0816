234da25a1f226b934ff83d90081e7148db07bc40
Jacob Nine <s527243@nwmissouri.edu>
2019-02-26 18:55:45 -0600

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -27,13 +27,13 @@ function offerExpires (today)
     var expiryMsg;
     var weekFromToday, day, date, month, year, dayNames, monthNames
     weekFromToday = new Date (today.getTime()+7*24*60*1000);
-    dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
+    dayNames = ["Sunday", "Monday", "Tuesday" , "Wednesday", "Thrusday", "Friday", "Saturday"];
     monthNames = ["Janurary", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     day = dayNames [weekFromToday.getDay()];
     date = weekFromToday.getDate();
     month = monthNames [weekFromToday.getMonth()];
     year = weekFromToday.getFullYear();
-    expiryMsg = "offer expires next";
+    expiryMsg = "offer expires next ";
     expiryMsg += day + "<br>"+( + date + " " + month + " " + year) ;
     return expiryMsg;
 }
